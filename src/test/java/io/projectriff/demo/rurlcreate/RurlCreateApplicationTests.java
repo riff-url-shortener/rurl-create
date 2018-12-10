0ddcfcfa458ae88494d50a8166ce1df1d79e4338
Swapnil Bawaskar <sbawaskar@pivotal.io>
2018-12-10 14:29:43 -0800

    initial code commit

---
+++ src/test/java/io/projectriff/demo/rurlcreate/RurlCreateApplicationTests.java
@@ -0,0 +1,16 @@
+package io.projectriff.demo.rurlcreate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RurlCreateApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
